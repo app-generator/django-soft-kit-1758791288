0a0c11345fff7b6b2e6eb9afd917ceb1ac0126d5
App Generator <51070104+app-generator@users.noreply.github.com>
2025-09-25 12:08:17 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    phone = models.TextField(max_length=255, null=True, blank=True)
+    name = models.TextField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Product(models.Model):
+
+    #__Product_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(max_length=255, null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    category = models.TextField(max_length=255, null=True, blank=True)
+    stock = models.IntegerField(null=True, blank=True)
+    sold = models.BooleanField()
+
+    #__Product_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Product")
+        verbose_name_plural = _("Product")
+
+
+
+#__MODELS__END
